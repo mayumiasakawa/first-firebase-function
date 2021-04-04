c0b0d9c4e3c3a0197e304f69d257d97ce434a906
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-04-04 23:50:41 +0900

    add to functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
+const admin = require('firebase-admin');
 const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+admin.initializeApp();
+var db = admin.firestore();
+
+exports.helloWorld = functions.https.onRequest(
+  (request,response) => {
+    db.collection('people').get()
+    .then((snapshot)=> {
+      var res = [];
+      snapshot.forEach((doc) => {
+        res.push(doc.data());
+      });
+      response.send(JSON.stringify(res));
+    })
+    .catch((err)=> {
+      response.send(err.localizedDescription);
+    });
+  }
+)
